e247e3ddf3f6296e1770dee8c9335191dfc505fb
Xena <xena@maayah.com>
2022-04-23 20:34:41 -0400

    changed next() to add a conditional, checking if the checkpoint has been visited before and thus disabling the pop up

--- static/learn.js
+++ static/learn.js
@@ -1,65 +1,64 @@
-$(document).ready(function(){
-    $("#nextbutton").click(function(){
+$(document).ready(function () {
+    $("#nextbutton").click(function () {
         next()
     });
+
 
     let navid = "#" + content.id;
 
     $(navid).addClass("active");
 
     makeCheckpoint();
-    
+    console.log(user)
 });
 
 function next() {
-
     if (content.checkpoint) {
+        if (user.checkpoint[content.id - 3] == 0) {
+            let confirmdiv = $('<div id="dialog-confirm" title="">')
+            let p = $("<p>")
+            let spans = $('<span class="ui-icon ui-icon-alert" style="float:left; margin:12px 12px 20px 0;"></span>')
+            $(p).append(spans)
+            $(p).append("You haven't completed the checkpoint yet. Are you sure you want to continue?")
+            $(confirmdiv).append(p)
+            $("#confirm").append(confirmdiv)
 
-        let confirmdiv = $('<div id="dialog-confirm" title="">')
-        let p = $("<p>")
-        let spans = $('<span class="ui-icon ui-icon-alert" style="float:left; margin:12px 12px 20px 0;"></span>')
-        $(p).append(spans)
-        $(p).append("You haven't completed the checkpoint yet. Are you sure you want to continue?")
-        $(confirmdiv).append(p)
-        $("#confirm").append(confirmdiv)
+            $(function () {
+                $("#dialog-confirm").dialog({
+                    resizable: false,
+                    height: "auto",
+                    width: 400,
+                    modal: true,
+                    buttons: {
+                        "Yes, continue": function () {
+                            location.href = content.nextid;
+                        },
+                        Cancel: function () {
+                            $(this).dialog("close");
 
-        $( function() {
-            $( "#dialog-confirm" ).dialog({
-                resizable: false,
-                height: "auto",
-                width: 400,
-                modal: true,
-                buttons: {
-                "Yes, continue": function() {
-                    location.href = content.nextid;
-                },
-                Cancel: function() {
-                    $( this ).dialog( "close" );
+                        }
 
-                }
+                    }
 
-                }
+                });
 
             });
-
-        });
-
+        } else {
+            location.href = content.nextid;
+        }
     } else {
         location.href = content.nextid;
     }
-
 }
 
 function makeCheckpoint() {
 
     if (content.checkpoint) {
-
         let buttondiv = $('<button class="alata greenBackground whiteText startQuiz smallPadding mediumText" id="checkpoint">Checkpoint</button>');
         $("#checkpoint").append(buttondiv);
-        $("#checkpoint").click(function(){
-            checkpoint()
+        $("#checkpoint").click(function () {
+            checkpoint();
         });
-
     }
 
 }
